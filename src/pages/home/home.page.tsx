90aefd996d8060bd4f8b04bb0f9d164ff16ad1a1
AdryDev <adrydevmateo@gmail.com>
2024-06-18 21:56:10 -0400

    Moved header to the bottom of the page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,7 +2,16 @@ export default function Home() {
 
  return (
   <div id="home-page">
-   <h1>Home</h1>
+   <div className="hero">
+    <h1>Resume Builder</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eius enim esse eum consectetur porro deleniti sint labore quis velit quidem a saepe voluptate, earum non commodi voluptas assumenda similique?</p>
+    <a href="/">Build Your Resume</a>
+   </div>
+
+   <div className="how-to-use">
+    <h2>Lorem Ipsum Dolor</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque autem perferendis nesciunt saepe facere maxime repudiandae quod architecto possimus nam? Beatae eius enim placeat tenetur vel quis explicabo ad officia!</p>
+   </div>
   </div>
  )
 }
