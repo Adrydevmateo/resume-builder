8c5653b48abef5acfb743f728984349edc00eef5
AdryDev <adrydevmateo@gmail.com>
2024-06-18 21:45:14 -0400

    Changed header layout at home layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,26 @@
+import './Header.css'
 import LanguageSwitch from "../../language/Language-Switch"
+import { Link } from "react-router-dom"
 
-interface Props {
- children: React.ReactNode
-}
-
-export default function Header({ children }: Props) {
+export default function Header() {
  return (
-  <header>
-   <div className="content">
-    {children}
-   </div>
+  <header id='header-comp'>
+   <nav>
+    <Link to="/">
+     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M4 19v-9q0-.475.213-.9t.587-.7l6-4.5q.525-.4 1.2-.4t1.2.4l6 4.5q.375.275.588.7T20 10v9q0 .825-.588 1.413T18 21h-3q-.425 0-.712-.288T14 20v-5q0-.425-.288-.712T13 14h-2q-.425 0-.712.288T10 15v5q0 .425-.288.713T9 21H6q-.825 0-1.412-.587T4 19" /></svg>
+    </Link>
+    <Link to="/sing-in">
+     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 12q-1.65 0-2.825-1.175T8 8t1.175-2.825T12 4t2.825 1.175T16 8t-1.175 2.825T12 12m-8 8v-2.8q0-.85.438-1.562T5.6 14.55q1.55-.775 3.15-1.162T12 13t3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2V20z" /></svg>
+    </Link>
+   </nav>
 
    <LanguageSwitch />
 
-   <div className="hamburger-wrapper">
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5" d="M20 7H4m16 5H4m16 5H4" /></svg>
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z" /></svg>
-   </div>
+   <label htmlFor="hamburger" className="hamburger-wrapper">
+    <svg className='hamburger-icon' xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="1.5" d="M20 7H4m16 5H4m16 5H4" /></svg>
+    <svg className='close-icon' xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z" /></svg>
+    <input type="checkbox" name="hamburger" id="hamburger" hidden />
+   </label>
   </header>
  )
 }
--- src/layouts/Home.layout.tsx
+++ src/layouts/Home.layout.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import './Home.style.css'
 import Header from "../components/header/Header"
 
 interface Props {
@@ -8,13 +8,7 @@ interface Props {
 export default function HomeLayout({ children }: Props) {
  return (
   <div id="home-layout">
-   <Header>
-    <nav>
-     <Link to="/">Home</Link>
-     <Link to="/sing-in">Sign In</Link>
-     <Link to="/sing-up">Sign Up</Link>
-    </nav>
-   </Header>
+   <Header></Header>
 
    {children}
   </div>
